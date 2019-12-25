e1421c4710d5b3b440d24f80433892c41b99ff4e
root <root@localhost.localdomain>
2019-12-25 13:56:53 +0700

    Membuat Aplikasi Android Notes Sederhana

---
+++ app/src/main/java/com/svafvel/software/production/mynotesapp/db/DatabaseContract.java
@@ -0,0 +1,16 @@
+package com.svafvel.software.production.mynotesapp.db;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    static String TABLE_NAME = "note";
+
+    public static final class NoteColumns implements BaseColumns{
+
+        public static String TITLE = "title";
+        public static String DESCRIPTION = "description";
+        public static String DATE = "date";
+    }
+
+}
---
+++ app/src/main/java/com/svafvel/software/production/mynotesapp/helper/MappingHelper.java
@@ -0,0 +1,30 @@
+package com.svafvel.software.production.mynotesapp.helper;
+
+import android.database.Cursor;
+
+import com.svafvel.software.production.mynotesapp.db.DatabaseContract;
+import com.svafvel.software.production.mynotesapp.entity.Note;
+
+import java.util.ArrayList;
+
+public class MappingHelper {
+
+    public static ArrayList<Note> mapCursorToArrayList(Cursor notesCursor){
+
+        ArrayList<Note> notesList = new ArrayList<>();
+
+        while (notesCursor.moveToNext()){
+
+            int id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID));
+            String title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE));
+            String description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION));
+            String date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE));
+
+        }
+
+        return notesList;
+
+
+    }
+
+}
